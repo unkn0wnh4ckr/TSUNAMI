f8aa93b6fef856062273206794b4d5d1c5fb58d0
un_kn0wn h4c_k3r <40415158+unkn0wnh4ckr@users.noreply.github.com>
2018-07-29 14:01:41 -0400

    Update update.py

--- update.py
+++ update.py
@@ -23,10 +23,7 @@ print G+"updating TSUNAMI"
 print N+"update started on {0}.{1} | {2}-{3}-{4}".format(hour, minute, day, month, year)
 time.sleep(2)
 
-os.system("rm TSUNAMI.py")
-os.system("rm install.py")
-os.system("rm README.md")
-os.system("rm update.py")
+os.system("cd")
 os.system("rm -fr TSUNAMI")
 
 time.sleep(1)
